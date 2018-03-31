1ba7bc114b65ad55309d988bca50687f78fdf35d
kovarnyirus <kovarnyi@gmail.com>
2018-03-31 14:20:02 +0300

    отслеживание комбинации клавиш

--- js/main.js
+++ js/main.js
@@ -1,29 +1,40 @@
-const initialTemplate = document.querySelector(`.central`);
+const INITIAL_TEMPLATE = document.querySelector(`.central`);
 const SCREEN_TEMPLATES = document.querySelectorAll(`template`);
-const AltKeyCode = 18;
-const ArrowLeftKeyCode = 37;
-const ArrowRightKeyCode = 39;
-let codes = null;
+const ORDER_TEMPLATES = {
+  'greeting': 1,
+  'rules': 2,
+  'game-1': 3,
+  'game-2': 4,
+  'game-3': 5,
+  'stats': 6
+};
+const ARROW_LEFT_KEYCODE = 37;
+const ARROW_RIGHT_KEYCODE = 39;
 let numberTemplate = 0;
 
+function sortTemplate(template1, template2) {
+console.log(template1)
+};
+
+
 const showTemplate = (numberTemplate) => {
-  initialTemplate.innerHTML = ``;
-  initialTemplate.appendChild(SCREEN_TEMPLATES[numberTemplate].content.cloneNode(true));
+  INITIAL_TEMPLATE.innerHTML = ``;
+  INITIAL_TEMPLATE.appendChild(SCREEN_TEMPLATES[numberTemplate].content.cloneNode(true));
 };
 
 function onTemplateControl(event) {
-  if (codes === AltKeyCode && event.keyCode === ArrowRightKeyCode) {
+  if (event.altKey && event.keyCode === ARROW_RIGHT_KEYCODE) {
     if (numberTemplate < 5) {
       ++numberTemplate;
     }
     showTemplate(numberTemplate);
-  } else if (codes === AltKeyCode && event.keyCode === ArrowLeftKeyCode) {
+  } else if (event.altKey && event.keyCode === ARROW_LEFT_KEYCODE) {
     if (numberTemplate > 0) {
       --numberTemplate;
     }
     showTemplate(numberTemplate);
   }
-  codes = event.keyCode;
 }
 
 document.addEventListener(`keydown`, onTemplateControl);
+console.log(SCREEN_TEMPLATES);
