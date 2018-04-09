a63289aafebeca5ed255df50260b479c7ab4b4af
kovarnyirus <kovarnyi@gmail.com>
2018-04-09 10:58:07 +0300

    module3-task2

--- js/data/timer.js
+++ js/data/timer.js
@@ -1,3 +1,21 @@
-export const timer (time) => {
+export const timer = (time) => {
+  if (typeof time !== `number`) {
+    throw new Error(`time must be a number`);
+  }
 
+  if (time < 0) {
+    throw new Error(`time cannot be negative`);
+  }
+
+  return {
+    value: time,
+    tick() {
+      if (time > 0) {
+        this.value--;
+      } else if (time === 0) {
+        return `Время вышло`;
+      }
+      return this.value;
+    }
+  };
 };
