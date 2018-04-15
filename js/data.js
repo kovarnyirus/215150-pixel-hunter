f6a120796b40bf4d8cc00505e3b1d225fa436568
kovarnyirus <kovarnyi@gmail.com>
2018-04-15 14:26:49 +0300

    save

--- js/data.js
+++ js/data.js
@@ -5,52 +5,53 @@ const INITIAL_STATE = {
 
 
 const state = {
-  gameOne: {
-    type: `game-1`,
-    title: `Угадайте для каждого изображения фото или рисунок?`,
-    images: [
-      {
-        src: `https://k42.kn3.net/CF42609C8.jpg`,
-        type: `paint`
-      },
-      {
-        src: `http://i.imgur.com/1KegWPz.jpg`,
-        type: `photo`
-      }
-    ]
-  },
-  gameTwo: {
-    type: `game-2`,
-    title: `Угадай, фото или рисунок?`,
-    images: [
-      {
-        src: `https://k42.kn3.net/D2F0370D6.jpg`,
-        type: `paint`
-      }
-    ]
-  },
-  gameThree: {
-    type: `game-3`,
-    title: `Найдите рисунок среди изображений`,
-    images: [
-      {
-        src: `https://k32.kn3.net/5C7060EC5.jpg`,
-        type: `paint`
-      },
-      {
-        src: `https://i.imgur.com/DiHM5Zb.jpg`,
-        type: `photo`
-      },
-      {
-        src: `http://i.imgur.com/DKR1HtB.jpg`,
-        type: `photo`
-      }
-    ]
-  },
   stats: [], // будет зполняться по мере игры
   lives: 3, // будет зполняться по мере игры
   answers: [], // будет зполняться по мере игры
-  level: 0,
+  levels: [
+    {
+      type: `game-1`,
+      title: `Угадайте для каждого изображения фото или рисунок?`,
+      images: [
+        {
+          src: `https://k42.kn3.net/CF42609C8.jpg`,
+          type: `paint`
+        },
+        {
+          src: `http://i.imgur.com/1KegWPz.jpg`,
+          type: `photo`
+        }
+      ]
+    },
+    {
+      type: `game-2`,
+      title: `Угадай, фото или рисунок?`,
+      images: [
+        {
+          src: `https://k42.kn3.net/D2F0370D6.jpg`,
+          type: `paint`
+        }
+      ]
+    },
+    {
+      type: `game-3`,
+      title: `Найдите рисунок среди изображений`,
+      images: [
+        {
+          src: `https://k32.kn3.net/5C7060EC5.jpg`,
+          type: `paint`
+        },
+        {
+          src: `https://i.imgur.com/DiHM5Zb.jpg`,
+          type: `photo`
+        },
+        {
+          src: `http://i.imgur.com/DKR1HtB.jpg`,
+          type: `photo`
+        }
+      ]
+    }
+  ],
   time: []
 };
 
