62117d23f3dd7229952e6273b0294621e897f1f1
kovarnyirus <kovarnyi@gmail.com>
2018-04-15 19:18:48 +0300

    save

--- js/data.js
+++ js/data.js
@@ -87,68 +87,24 @@ const INITIAL_STATE = {
   time: 0
 };
 
+const getRandomGame = () => {
+  let gameList = [];
+  for (let i = 0; i <= 10; i++) {
+    arr.push(arrGames[getRandom(2)]);
+  }
+  return gameList;
+};
 
 const state = {
   stats: [], // будет зполняться по мере игры
   lives: 3, // будет зполняться по мере игры
   answers: [], // будет зполняться по мере игры
-  levels: [
-    {
-      type: `game-1`,
-      title: `Угадайте для каждого изображения фото или рисунок?`,
-      images: [
-        {
-          src: `https://k42.kn3.net/CF42609C8.jpg`,
-          type: `paint`
-        },
-        {
-          src: `http://i.imgur.com/1KegWPz.jpg`,
-          type: `photo`
-        }
-      ]
-    },
-    {
-      type: `game-2`,
-      title: `Угадай, фото или рисунок?`,
-      images: [
-        {
-          src: `https://k42.kn3.net/D2F0370D6.jpg`,
-          type: `paint`
-        }
-      ]
-    },
-    {
-      type: `game-3`,
-      title: `Найдите рисунок среди изображений`,
-      images: [
-        {
-          src: `https://k32.kn3.net/5C7060EC5.jpg`,
-          type: `paint`
-        },
-        {
-          src: `https://i.imgur.com/DiHM5Zb.jpg`,
-          type: `photo`
-        },
-        {
-          src: `http://i.imgur.com/DKR1HtB.jpg`,
-          type: `photo`
-        }
-      ]
-
-    }
-  ],
-  levels2: [],
+  levels: getRandomGame(),
   currentLevel: 0,
   time: []
 };
 
-const getRandomGame = () => {
-  let gamesList = [];
-  for (let i = 0; i <= 10; i++) {
-    gamesList.push(arrGames[getRandom(2)]);
-  }
-  state.levels2 = gamesList;
-};
+
 console.log(state);
 getRandomGame();
 
